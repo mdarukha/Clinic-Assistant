b4a63030a698abab3c92c6233c26b26be71f03ad
mdarukha <137126403+mdarukha@users.noreply.github.com>
2025-08-02 16:58:53 -0700

    update app path

--- clinic_assistant_app.py
+++ clinic_assistant_app.py
@@ -1,7 +1,11 @@
 import streamlit as st
-from clinic_assistant_streamlit import handle_query
+from webapp.clinic_assistant_streamlit import handle_query
 import json
 import os
+
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 
 # use credentials.json from streamlit secrets
 if "GOOGLE_CREDENTIALS" in st.secrets and not os.path.exists("credentials.json"):
